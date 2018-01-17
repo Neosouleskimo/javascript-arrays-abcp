d3ca48b0681be319995a3aff21fc8031012fac5c
Christopher Miller <camillustration@gmail.com>
2018-01-17 03:45:11 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles" ];
+var superheroines = ["catwoman", "she-hulk", "mystique", ];
+
+superheroines.push( "wonder woman")
+superheroines.unshift("Tilda Swinton")
+
+var cities = ["New York", "San Francisco"]
+cities.unshift("Philadelphia")
+["Philadelphia", ...cities];
+
+cities
